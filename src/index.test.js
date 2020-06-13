dcc13482cdea6b753596fdd48a21c5de9321223f
kavinRobosoft <kavin.s@robosoftin.com>
2020-06-14 01:29:04 +0530

    adding mocha chai

---
+++ src/index.test.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+var countries = require('mocha');
+
+describe('country-names', () => {
+
+    it("should work", () => {
+        expect(true).to.be.true;
+    })
+})
